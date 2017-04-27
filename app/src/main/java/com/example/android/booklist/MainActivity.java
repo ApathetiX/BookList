0466fbebc1d389ebe9fe16449cce01cb07369830
ApathetiX <Ahmed.sameet@gmail.com>
2017-04-26 23:38:03 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/android/booklist/MainActivity.java
+++ app/src/main/java/com/example/android/booklist/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private BookAdapter mAdapter;
 
     /**
-     * Constant value for the earthquake loader ID. We can choose any integer.
+     * Constant value for the book loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
      */
     private static final int BOOK_LOADER_ID = 1;
